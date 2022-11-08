603ac2303201b9d24bfa9d691859f596e371bffc
B0bbycode <onaolapolekki@outlook.com>
2022-11-08 17:01:23 +0200

    Initial commit

---
+++ age.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int block,answer;
+	
+	cout<<"==How old are you? == \n";
+	cin>>block;
+	
+	answer = 2022 - block;
+	cout<<"you are born in year \n"<<answer;
+	
+	
+	return 0;
+}
